091b2138a6d26374c183acb2ed1694364f84cb14
Ian <istreator@yahoo.com>
2022-12-18 15:27:30 -0600

    created global types WIP.

---
+++ Types.ts
@@ -0,0 +1,10 @@
+export type sizes = {
+  thumb: string;
+  full: string;
+};
+export type Unsplash = {
+  urls: sizes;
+};
+export type Urls = sizes[];
+
+export type UrlSizes = { index: { thumb: string; full: string } };
