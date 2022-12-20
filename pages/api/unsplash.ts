bf5634dba7fa7d93cfdcf77f1a016e1c5ae71379
Ian <istreator@yahoo.com>
2022-12-20 02:18:04 -0600

    maxed out images returned per request, added uid from thumb urls for managing saved state

--- pages/api/unsplash.ts
+++ pages/api/unsplash.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Unsplash } from "../../Types";
+import { UidUrls, UrlSizes } from "../../Types";
 
 const BASE_API_URL = "https://api.unsplash.com/";
 const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
@@ -8,35 +8,37 @@ const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 interface Req extends NextApiRequest {
   body: string;
 }
+
+type Unsplash = {
+  urls: UrlSizes;
+};
+
 let TTR = 0;
 setInterval(() => {
   TTR -= 1;
 }, 1000);
 export default async function handler({ body }: Req, res: NextApiResponse) {
-  console.log(body);
-  console.log(TTR);
-
   try {
     if (TTR < 1) {
-      console.log("inside");
       TTR = 10;
+
       const unsplashRes = await fetch(
-        `${BASE_API_URL}search/photos?query=${body}&per_page=10&order_by=popular&client_id=${UNSPLASH_ACCESS_KEY}`
+        `${BASE_API_URL}search/photos?query=${body}&per_page=50&order_by=popular&client_id=${UNSPLASH_ACCESS_KEY}`
       );
       const { results } = (await unsplashRes.json()) as { results: Unsplash[] };
-      console.log(results);
-      const urls = results.map(({ urls: { thumb, full } }: Unsplash) => {
-        return { thumb, full };
+
+      const urlsObject = {} as UidUrls;
+      results.forEach(({ urls: { thumb, full } }: Unsplash) => {
+        const uid = thumb.split("?")[0].split("-")[1];
+        urlsObject[uid] = { thumb, full };
       });
-      console.log(urls);
-      res.status(200).send(urls);
+      res.status(200).send(urlsObject);
     } else {
-      res.status(429).send({
-        message: `you may send another request in ${TTR} seconds`,
-      });
+      res
+        .status(429)
+        .send({ message: `you may send another request in ${TTR} seconds` });
     }
   } catch (error) {
-    console.log(error);
     res.status(500).send(error);
   }
 }
